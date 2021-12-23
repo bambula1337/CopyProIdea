a7343527791b7df0d51b69b03c4cfc03484d8a54
Adm's <lemopidr1@gmail.com>
2021-12-23 21:29:59 +0100

    new header

--- cpproidea/src/components/CPHeader.vue
+++ cpproidea/src/components/CPHeader.vue
@@ -1,17 +1,16 @@
 <template>
   <header class="header">
+    <p class="logo">Skybox</p>
     <div class="navigation">
-      <router-link to="/" class="nav-link-default" :active-class="'active'" exact>Home</router-link
-      >
-      <router-link to="/about" class="nav-link-default" :active-class="'active'"
-        >About</router-link
-      >
-      <router-link
-        to="/default"
-        class="nav-link-default"
-        :active-class="'active'"
-        >Default</router-link
-      >
+      <div class="links">
+        <router-link to="/" class="nav-link-default" :active-class="'active'" exact>Home</router-link>
+        <router-link to="/pricing" class="nav-link-default" :active-class="'active'">Pricing</router-link>
+        <router-link to="/contact" class="nav-link-default" :active-class="'active'">Contact</router-link>
+      </div>
+      <div class="login">
+        <router-link to="/login" class="login-link" :active-class="'active'" exact>Login</router-link>
+        <router-link to="/signup" class="sign-up-link" :active-class="'active'"><button>Sign Up</button></router-link>
+      </div>
     </div>
   </header>
 </template>
@@ -24,15 +23,35 @@ export default {
 
 <style lang="scss" scoped>
 .header {
-  @apply flex justify-center items-center bg-transparent h-16 flex-col absolute top-0 left-0 z-10 w-full;
-  & p{
-    @apply text-purple-900 mb-3;
+  @apply flex justify-around items-center bg-transparent h-16 absolute top-0 left-0 z-10 w-full mt-3;
+
+  & .logo{
+    @apply text-3xl uppercase text-gray-50 font-light tracking-widest -mr-20;
   }
+
   & .navigation {
+    @apply flex;
+
+    & .links{
+      @apply mr-44;
+    }
     & .nav-link-default {
-      @apply mx-2 self-center text-purple-50;
+      @apply mx-12 text-purple-50 tracking-wider;
       &.active{
-        @apply text-purple-900;
+        @apply text-purple-50;
+      }
+    }
+    & .login{
+      @apply text-gray-50;
+
+      & .login-link{
+        @apply mr-16 uppercase tracking-widest;
+      }
+
+      & .sign-up-link{
+        & button{
+          @apply uppercase bg-gray-50 text-purple-700 rounded-full w-28 h-9 text-xs tracking-widest;
+        }
       }
     }
   }
